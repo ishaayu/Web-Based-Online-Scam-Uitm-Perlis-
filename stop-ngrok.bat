db399fddc847807bd7fad626df54c8adc1ecc3c5
ZakatGoldApp <ishaayu03@gmail.com>
2026-06-30 11:45:37 +0800

    remove local ngrok files

--- stop-ngrok.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Stopping ngrok...
-taskkill /F /IM ngrok.exe >nul 2>&1
-if errorlevel 1 (
-    echo No ngrok process was running.
-) else (
-    echo ngrok stopped. You can now run start-ngrok.bat again.
-)
